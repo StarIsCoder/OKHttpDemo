6a90168668ad943a968053fed48d2e4352c45fc5
star <654641639@qq.com>
2018-04-24 20:55:25 +0800

    update practice

--- app/src/main/java/com/example/star/okhttpdemo/MainActivity.java
+++ app/src/main/java/com/example/star/okhttpdemo/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.star.okhttpdemo;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.File;
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -18,6 +20,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "okhttp";
     private TextView textView;
     private String url = "http://192.168.8.101:8080/TestForCSDN/";
     //1、拿到OkHttpClient对象 可以设置全局信息 例如timeout
@@ -30,11 +33,30 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.get_textView);
     }
 
+    public void doPostFile(View view) {
+        //Post文件
+        File directory = Environment.getExternalStorageDirectory();
+        File file = new File(directory, "jay.jpg");
+        if (!file.exists()) {
+            Log.e(TAG, file.getAbsolutePath() + "is not found!");
+            return;
+        }
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/octet-stream"), file);
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(url + "postFile").post(requestBody).build();
+        try {
+            Log.d(TAG, "doPostFile start");
+            execRequest(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void doPost(View view) {
         //构造FormBody
         FormBody.Builder formBody = new FormBody.Builder();
-        formBody.add("username", "wangshenxing");
-        formBody.add("password", "star");
+        formBody.add("username", "star");
+        formBody.add("password", "123");
         Request.Builder builder = new Request.Builder();
         Request request = builder.url(url + "test").post(formBody.build()).build();
         try {
@@ -46,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void doPostString(View view) {
         RequestBody requestBody = RequestBody.create(MediaType.parse("text/plan;charset=utf-8"),
-                "{username:wangshenxing,password:123}");
+                "{username:star,password:123}");
         Request.Builder builder = new Request.Builder();
         Request request = builder.url(url + "postString").post(requestBody).build();
         try {
@@ -89,7 +111,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String result = response.body().string();
-                Log.d("wangshenxing", "response body :" + result);
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
